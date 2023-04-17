56e28983e41b16179bc3b4436b06cb352f4689d9
Mimi <hana.nguyen@outlook.fr>
2023-04-17 03:42:26 +0200

    Getting rid of old menu

--- controllers/match.py
+++ controllers/match.py
@@ -1,31 +1,17 @@
 from views.match import MatchView
 from tinydb import TinyDB
+from datetime import datetime
 
 db_tournaments = TinyDB('db_tournaments.json').table("tournaments")
 
 class MatchController:
     def __init__(self, rounds_number, current_round, tournament_doc_id):
+        self.current_time = datetime.now().strftime("%d/%m/%Y-%H:%M")
         self.view = MatchView()
         self.rounds_number = rounds_number
         self.current_round = current_round
         self.tournament_doc_id = tournament_doc_id
         self.tournament = db_tournaments.get(doc_id=self.tournament_doc_id)
-
-    def display_menu(self):
-        self.view.custom_print(f"\n\nRound {self.current_round} sur {self.rounds_number}")        
-        while True:
-            user_input = self.view.user_choice()
-            if user_input == "1":
-                self.write_score()
-            elif user_input == "2":
-                self.find_player()
-            elif user_input == "3":
-                self.delete_match()
-            elif user_input == "4":
-                return
-            else: 
-                self.view.custom_print("Erreur de sélection, veuillez sélectionner une option valide.")
-                self.display_menu()
     
     def find_player(self, chess_id, score):
         players = self.tournament["players"]
@@ -33,7 +19,8 @@ class MatchController:
             if player["chess_id"] == chess_id:
                 player["score"] = player["score"] + score
 
-    def write_score(self):
+    def display_menu(self):
+        self.view.custom_print(f"\nRound {self.current_round} sur {self.rounds_number}")
         round = self.tournament["rounds_list"][self.current_round-1]
         matches = round['matches']
     
@@ -41,7 +28,7 @@ class MatchController:
             player1 = match["players"][0]
             player2 = match["players"][1]
             match_name = match["name"]
-            self.view.custom_print(f"\n{match_name} : J1({player1}) vs J2({player2})\n")
+            self.view.custom_print(f"\n{match_name} : J1({player1}) vs J2({player2})")
             user_input = self.view.get_match_scores()
 
             if(user_input == "1"): #P1 wins
@@ -61,12 +48,7 @@ class MatchController:
                 return
             else:
                 self.view.custom_print("Veuillez choisir une option valide")
-                self.write_score()
+                self.display_menu()
 
+        round["end_time"] = self.current_time
         db_tournaments.update(self.tournament, doc_ids=[self.tournament_doc_id])
-    
-    def update_match(self):
-        pass
-
-    def delete_match(self):
-        pass
